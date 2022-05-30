c138d75f0ede3a40904358335fdf05c572241e12
carter-vin <cartervincent684@gmail.com>
2022-05-30 20:41:09 +0545

    bugfix: update the colors of react-select update the finance data not loading

--- src/components/MultipleSelect.tsx
+++ src/components/MultipleSelect.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-expressions */
-import { Box, InputLabel, FormHelperText } from '@mui/material';
+import { Box, InputLabel, FormHelperText, useTheme } from '@mui/material';
 import React from 'react';
 import ReactSelect from 'react-select';
 import { OptionType } from 'utils/select';
@@ -16,12 +17,52 @@ interface MultipleSelectProps {
 
 const MultipleSelect = (props: MultipleSelectProps) => {
     const { label, name, options, values, error, onChange, disabled } = props;
+    const theme = useTheme();
+    const reactSelectStyles = {
+        control: (provided: any) => ({
+            ...provided,
+            backgroundColor: 'transparent',
+            minHeight: '38px',
+            border:
+                theme.palette.mode === 'dark'
+                    ? '1px solid #fff'
+                    : '1px solid #1A202C',
+            boxShadow: 'none',
+        }),
 
+        valueContainer: (provided: any) => ({
+            ...provided,
+            padding: '0 6px',
+        }),
+
+        indicatorSeparator: () => ({
+            display: 'none',
+        }),
+        indicatorsContainer: (provided: any) => ({
+            ...provided,
+            height: '32px',
+            marginTop: -2,
+        }),
+        menu: (provided: any) => ({
+            ...provided,
+            zIndex: 9999,
+            backgroundColor:
+                theme.palette.mode === 'dark' ? '#212839' : 'white',
+        }),
+        option: (provided: any, state: any) => ({
+            ...provided,
+            '&:hover': {
+                borderColor: 'red',
+                color: state.isFocused ? 'black' : 'white',
+            },
+        }),
+        menuPortal: (base: any) => ({ ...base, zIndex: 9999 }),
+    };
     return (
         <Box display="flex" flexDirection="column" gap={1}>
             {label && (
                 <Box>
-                    <InputLabel htmlFor="email">
+                    <InputLabel htmlFor="email" className="capitalize">
                         <strong className="text-gray-700"> {label} </strong>
                     </InputLabel>
                 </Box>
@@ -38,6 +79,8 @@ const MultipleSelect = (props: MultipleSelectProps) => {
                         ? onChange(options.slice(1))
                         : onChange(selected);
                 }}
+                menuPortalTarget={document.body}
+                styles={reactSelectStyles}
             />
             {Boolean(error) && (
                 <FormHelperText error id="username" color="red">
--- src/modules/vendors/components/VendorFinance.tsx
+++ src/modules/vendors/components/VendorFinance.tsx
@@ -14,6 +14,15 @@ import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 const VendorFinance = (props: { vendorFinance: any }) => {
     const { vendorFinance } = props;
+    const getFinanceDocumentUrl = async (contract: any) => {
+        const res = await Storage.get(
+            contract?.key || contract?.files[0]?.name,
+            {
+                level: 'protected',
+            }
+        );
+        return res;
+    };
     return (
         <CardContent>
             <Stack direction="column" spacing={2}>
@@ -35,54 +44,49 @@ const VendorFinance = (props: { vendorFinance: any }) => {
                     )}
                     <Divider />
                     {!vendorFinance.financeTaggedUser &&
-                        map(
-                            vendorFinance?.contracts,
-                            async (contract, index) => {
-                                const downloadUrl = await Storage.get(
-                                    contract?.key || contract?.files[0]?.name,
-                                    {
-                                        level: 'protected',
-                                    }
-                                );
-                                return (
-                                    <Grid
-                                        container
-                                        spacing={2}
-                                        key={`${contract.type}-${index}-${contract?.key}`}
-                                    >
-                                        <Grid item xs={4}>
-                                            <Typography className="capitalize">
-                                                Contract Type
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Box>
-                                                <Typography className="capitalize">
-                                                    {contract?.type}
-                                                </Typography>
-                                                {(contract?.files &&
-                                                    contract?.files[0]?.name) ||
-                                                    (contract?.key && (
-                                                        <Button
-                                                            startIcon={
-                                                                <AttachFileIcon />
-                                                            }
-                                                            href={downloadUrl}
-                                                        >
-                                                            <Typography>
-                                                                {contract?.key ||
-                                                                    contract
-                                                                        ?.files[0]
-                                                                        ?.name}
-                                                            </Typography>
-                                                        </Button>
-                                                    ))}
-                                            </Box>
-                                        </Grid>
+                        map(vendorFinance?.contracts, (contract, index) => {
+                            return (
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    key={`${contract.type}-${index}-${contract?.key}`}
+                                >
+                                    <Grid item xs={4}>
+                                        <Typography className="capitalize">
+                                            Contract Type
+                                        </Typography>
                                     </Grid>
-                                );
-                            }
-                        )}
+                                    <Grid item xs={6}>
+                                        <Box>
+                                            <Typography className="capitalize">
+                                                {contract?.type}
+                                            </Typography>
+                                            {(contract?.files &&
+                                                contract?.files[0]?.name) ||
+                                                (contract?.key && (
+                                                    <Button
+                                                        startIcon={
+                                                            <AttachFileIcon />
+                                                        }
+                                                        href={
+                                                            getFinanceDocumentUrl(
+                                                                contract
+                                                            ) || ''
+                                                        }
+                                                    >
+                                                        <Typography>
+                                                            {contract?.key ||
+                                                                contract
+                                                                    ?.files[0]
+                                                                    ?.name}
+                                                        </Typography>
+                                                    </Button>
+                                                ))}
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+                            );
+                        })}
                 </Box>
             </Stack>
         </CardContent>
